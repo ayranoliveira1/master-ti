a7f3070bd8a6458064147d1182010da2528e5f33
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-07-01 19:51:56 -0300

    Feat: add container to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,39 +2,40 @@ import Image from "next/image";
 
 const Footer = () => {
    return (
-      <div className="flex px-36 gap-5 py-5 justify-between items-center text-white bg-[#292734]">
-         <div className="flex gap-5 items-center">
-            <Image
-               src="/logoMaster.png"
-               alt="logo"
-               width={60}
-               height={60}
-               className="object-cover"
-            />
+      <footer className=" text-white bg-[#292734] px-36 py-5">
+         <div className="container mx-auto flex gap-5 justify-between items-center text-white bg-[#292734]">
+            <div className="flex gap-5 items-center">
+               <Image
+                  src="/logoMaster.png"
+                  alt="logo"
+                  width={60}
+                  height={60}
+                  className="object-cover"
+               />
+               <p className="text-sm">
+                  © 2024 Master TI, Todos os Direitos Reservados
+               </p>
+            </div>
             <p className="text-sm">
-               © 2024 Master TI, Todos os Direitos Reservados
+               Desenvolvido por{" "}
+               <a
+                  className="hover:underline"
+                  href="https://www.linkedin.com/in/ayran-oliveira-258499291/"
+                  target="_blank"
+               >
+                  Ayran
+               </a>{" "}
+               /{" "}
+               <a
+                  className="hover:underline"
+                  href="https://www.linkedin.com/in/lucio-gabriel-6a9887276/"
+                  target="_blank"
+               >
+                  Gabriel
+               </a>
             </p>
          </div>
-
-         <p className="text-sm">
-            Desenvolvido por{" "}
-            <a
-               className="hover:underline"
-               href="https://www.linkedin.com/in/ayran-oliveira-258499291/"
-               target="_blank"
-            >
-               Ayran
-            </a>{" "}
-            /{" "}
-            <a
-               className="hover:underline"
-               href="https://www.linkedin.com/in/lucio-gabriel-6a9887276/"
-               target="_blank"
-            >
-               Gabriel
-            </a>
-         </p>
-      </div>
+      </footer>
    );
 };
 
