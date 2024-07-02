cdd6302b35a91570e725a1c06f640dd708ce6d8f
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-07-02 18:16:20 -0300

    Refactor: changing image size and text font

--- components/start.tsx
+++ components/start.tsx
@@ -12,19 +12,19 @@ const Start = () => {
             <Image
                src={"/main.png"}
                alt="Logo"
-               width={200}
-               height={200}
+               width={210}
+               height={210}
                className="object-cotain"
             />
 
-            <h1 className="text-white bg-[#292734] text-3xl p-5 rounded-2xl">
+            <h1 className="text-white font-medium bg-black text-3xl px-5 py-8 rounded-2xl">
                Melhor Empresa de Desenvolvimento de Sites <br /> e Soluções Para
                Seu Negocio Local & Online!
             </h1>
          </div>
 
          <div className="flex flex-col gap-2">
-            <p className="text-white text-xs">
+            <p className="text-md font-medium">
                Dois amigos apaixonados por design e programação unem forças em
                2024 para <br /> fundar a Master TI, uma empresa especializada em
                criar sites personalizados. <br /> Com uma abordagem artesanal e
@@ -39,8 +39,9 @@ const Start = () => {
                   "https://api.whatsapp.com/send?phone=+55(91)982534512&text=Olá%2C+gostaria+de+conhecer+mais+o+trabalho+de+sites+de+vocês!"
                }
                target="_blank"
+               className="mx-auto"
             >
-               <Button className="bg-[#292734] rounded-2xl text-sm w-fit">
+               <Button className="bg-black rounded-2xl text-sm w-fit">
                   Consulte Mais Informações{" "}
                   <FaWhatsapp size={24} className="pl-2" />
                </Button>
